915710d9972ac523f31ad2038c1b5465c49d4d86
김형민 <kimtuti@naver.com>
2024-04-22 17:56:36 +0900

    feat: 유저에서 사용하는 병원 응답 객체 생성 (#3)

--- api/src/main/java/org/egglog/api/hospital/model/entity/Hospital.java
+++ api/src/main/java/org/egglog/api/hospital/model/entity/Hospital.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.egglog.api.user.model.entity.Users;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -17,28 +20,37 @@ public class Hospital {
     @Column(name = "hospital_id")
     private Long id;
 
+    @Column(name = "sido_code")
     private String sidoCode;
 
+    @Column(name = "sido")
     private String sido;
 
+    @Column(name = "gungu_code")
     private String gunguCode;
 
+    @Column(name = "gungu")
     private String gungu;
 
+    @Column(name = "dong")
     private String dong;
 
+    @Column(name = "zip_code")
     private String zipCode;
 
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "hospital_name")
     private String hospitalName;
 
+    @Column(name = "lat")
     private String lat;  //위도
 
+    @Column(name = "lng")
     private String lng;  //경도
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private Users user;
+    @OneToMany(mappedBy = "hospital")
+    private List<Users> users = new ArrayList<>();
 
 }
--- api/src/main/java/org/egglog/api/hospital/model/repository/HospitalJpaRepository.java
+++ api/src/main/java/org/egglog/api/hospital/model/repository/HospitalJpaRepository.java
@@ -1,8 +1,8 @@
-package org.egglog.api.hospital.model.repository;
-
-import org.egglog.api.board.model.entity.Board;
-import org.egglog.api.hospital.model.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalJpaRepository extends JpaRepository<Hospital, Long> {
-}
+//package org.egglog.api.hospital.model.repository;
+//
+//import org.egglog.api.board.model.entity.Board;
+//import org.egglog.api.hospital.model.entity.Hospital;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface HospitalJpaRepository extends JpaRepository<Hospital, Long> {
+//}
