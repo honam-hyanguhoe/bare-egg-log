5db7c7cb2c273124ecb0d46d2bb866006e032f90
김형민 <kimtuti@naver.com>
2024-04-24 02:24:09 +0900

    feat : 이전 코드 삭제 (#3)

--- api/src/main/java/org/egglog/api/security/repository/EmailCertificationRepository.java
+++
@@ -1,8 +0,0 @@
-//package org.egglog.api.security.repository;
-//
-//import com.nursetest.app.security.model.entity.CertificationNumber;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface EmailCertificationRepository extends CrudRepository<CertificationNumber, String> {
-//
-//}
