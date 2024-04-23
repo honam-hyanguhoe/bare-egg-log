0bcc484a17b13c3bd81979d2995ab03207d17fe0
kimahyeon <dhflxhdxhd@gmail.com>
2024-04-23 14:34:30 +0900

    feat: result card componets #8

--- client/app/src/main/java/com/org/egglog/client/MainActivity.kt
+++ client/app/src/main/java/com/org/egglog/client/MainActivity.kt
@@ -39,11 +39,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.org.egglog.client.ui.atoms.cards.ResultCard
 import com.org.egglog.client.ui.theme.ClientTheme
 import com.org.egglog.client.ui.theme.Typography
 import com.org.egglog.client.ui.atoms.icons.Icon
 import com.org.egglog.client.ui.atoms.imageLoader.LocalImageLoader
 import com.org.egglog.client.ui.atoms.imageLoader.UrlImageLoader
+import com.org.egglog.client.utils.MessageUtil
 import com.org.egglog.client.utils.Notification
 
 class MainActivity : ComponentActivity() {
@@ -88,9 +90,13 @@ fun OnboardingScreen(
 //            Text("Continue")
             Icon(Notification, modifier = Modifier.size(25.dp))
         }
-        Image(painter = painterResource(id = R.drawable.bottom_logo), contentDescription = null)
-        UrlImageLoader(imageUrl = "https://picsum.photos/300", modifier = Modifier.size(300.dp))
+//        Image(painter = painterResource(id = R.drawable.bottom_logo), contentDescription = null)
+//        UrlImageLoader(imageUrl = "https://picsum.photos/300", modifier = Modifier.size(300.dp))
         LocalImageLoader(imageUrl = R.drawable.off)
+        ResultCard(message = MessageUtil.APPROVE)
+        ResultCard(message = MessageUtil.APPLY)
+        ResultCard(message = MessageUtil.REGISTER)
+        ResultCard(message = MessageUtil.NO_SEARCH_RESULT)
     }
 }
 
