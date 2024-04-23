188bda2c7608d41d8dcfff455d35ebfa4ea4beb2
김수린 <uhyeon1009@gmail.com>
2024-04-23 17:59:49 +0900

    feat: button 작업 중

--- client/app/src/main/java/com/org/egglog/client/MainActivity.kt
+++ client/app/src/main/java/com/org/egglog/client/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.org.egglog.client
 
-import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
@@ -9,12 +9,10 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -24,11 +22,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,19 +37,34 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.org.egglog.client.ui.atoms.buttons.AuthButton
+import com.org.egglog.client.ui.atoms.buttons.BigButton
+import com.org.egglog.client.ui.atoms.buttons.GroupButton
+import com.org.egglog.client.ui.atoms.buttons.HalfBigButton
+import com.org.egglog.client.ui.atoms.buttons.HalfMiddleButton
+import com.org.egglog.client.ui.atoms.buttons.HalfThinButton
+import com.org.egglog.client.ui.atoms.buttons.MiddleButton
+import com.org.egglog.client.ui.atoms.buttons.ProfileButton
+import com.org.egglog.client.ui.atoms.buttons.ThinButton
+import com.org.egglog.client.ui.atoms.icons.Icon
 import com.org.egglog.client.ui.theme.ClientTheme
 import com.org.egglog.client.ui.theme.Typography
 import com.org.egglog.client.ui.atoms.imageLoader.LocalImageLoader
 import com.org.egglog.client.ui.atoms.imageLoader.UrlImageLoader
 import com.org.egglog.client.utils.widthPercent
-import com.org.egglog.client.ui.atoms.labels.Labels
-import com.org.egglog.client.utils.Notification
+import com.org.egglog.client.ui.theme.*
+import com.org.egglog.client.utils.AddBox
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,15 +79,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-    Surface(modifier, color = MaterialTheme.colorScheme.background) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings()
-        }
-    }
+//    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+//
+//    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+//        if (shouldShowOnboarding) {
+//            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+//        } else {
+//            Greetings()
+//        }
+//    }
+    ButtonTest()
 }
 
 @Composable
@@ -98,27 +112,6 @@ fun OnboardingScreen(
         Image(painter = painterResource(id = R.drawable.bottom_logo), contentDescription = null)
         UrlImageLoader(imageUrl = "https://picsum.photos/300", modifier = Modifier.size(320.widthPercent(LocalContext.current).dp))
         LocalImageLoader(imageUrl = R.drawable.off)
-
-        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            Labels(text = "Day",size = "big")
-            Labels(text = "Eve",size = "big")
-            Labels(text = "Night",size = "big")
-            Labels(text = "교육",size = "big")
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            Labels(text = "Eve")
-            Labels(text = "Off")
-            Labels(text = "Night")
-            Labels(text = "Eve")
-            Labels(text = "보건")
-            Labels(text = "휴가")
-            Labels(text= "None")
-        }
-
-
     }
 }
 
@@ -140,7 +133,9 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primary
         ),
-        modifier = modifier.size(320.dp).padding(vertical = 4.widthPercent(LocalContext.current).dp, horizontal = 8.dp)
+        modifier = modifier
+            .size(320.dp)
+            .padding(vertical = 4.widthPercent(LocalContext.current).dp, horizontal = 8.dp)
     ) {
         CardContent(name)
     }
@@ -152,18 +147,18 @@ private fun CardContent(name: String) {
 
     Row(
         modifier = Modifier
-                .padding(12.dp)
-                .animateContentSize(
-                        animationSpec = spring(
-                                dampingRatio = Spring.DampingRatioMediumBouncy,
-                                stiffness = Spring.StiffnessLow
-                        )
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
+            )
     ) {
         Column(
             modifier = Modifier
-                    .weight(1f)
-                    .padding(12.dp)
+                .weight(1f)
+                .padding(12.dp)
         ) {
             Text(text = "Hello, ")
             Text(
@@ -196,13 +191,138 @@ private fun CardContent(name: String) {
 //    uiMode = Configuration.UI_MODE_NIGHT_YES,
 //    name = "DefaultPreviewDark"
 //)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    name = "DefaultPreviewLight"
-)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_NO,
+//    name = "DefaultPreviewLight"
+//)
 @Composable
 fun MyAppPreview() {
     ClientTheme {
         MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ButtonTest(modifier: Modifier = Modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+        Column(modifier = modifier.fillMaxSize()) {
+            BigButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Warning25,
+                    containerColor = Warning300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Text(
+                    style = Typography.bodyLarge,
+                    text = "회원가입 완료하기"
+                )
+            }
+            MiddleButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Warning25,
+                    containerColor = Warning300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Row(Modifier.fillMaxSize(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
+                    Text(
+                        style = Typography.displayLarge,
+                        text = "그룹을 만들고 동료를 초대해보세요"
+                    )
+                    Icon(
+                        AddBox,
+                        Modifier.size(24.widthPercent(LocalContext.current).dp),
+                        NaturalWhite
+                    )
+                }
+            }
+
+            ThinButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Warning25,
+                    containerColor = Warning300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Text(
+                    style = Typography.labelLarge,
+                    text = "근무표 등록하기"
+                )
+            }
+
+            HalfBigButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Gray25,
+                    containerColor = Gray300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Text(
+                    style = Typography.displayLarge,
+                    text = "취소"
+                )
+            }
+
+            HalfMiddleButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Gray800,
+                    containerColor = Gray300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Text(
+                    style = Typography.displayLarge,
+                    text = "취소"
+                )
+            }
+
+            HalfThinButton(
+                onClick = { Log.d("clicked: ", "clicked!!!!")},
+                colors = ButtonColors(
+                    contentColor = Gray25,
+                    containerColor = Gray300,
+                    disabledContentColor = Gray25,
+                    disabledContainerColor = Gray300
+                )) {
+                Text(
+                    style = Typography.displayLarge,
+                    text = "취소"
+                )
+            }
+
+//            AuthButton(onClick = {Log.d("test: ", "clicked!!!")}, type = "kakao")
+//            AuthButton(onClick = {Log.d("test: ", "clicked!!!")}, type = "naver")
+//            AuthButton(onClick = {Log.d("test: ", "clicked!!!")}, type = "google")
+            GroupButton(onClick = {Log.d("test: ", "clicked!!!")}, groupMaster = "김다희", groupName = "호남향우회", memberCnt = 1, groupImage = 1, groupId = 1)
+            ProfileButton(onClick = {Log.d("test: ", "clicked!!!")}, profileImgUrl = "https://picsum.photos/300", isMine = true, isSelected = true, userId = 1, userName = "김호남")
+            ProfileButton(onClick = {Log.d("test: ", "clicked!!!")}, profileImgUrl = "https://picsum.photos/300", isMine = false, isSelected = true, userId = 1, userName = "김호남")
+            val stroke = Stroke(width = 2f,
+                pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+            )
+            Box(
+                Modifier
+                    .size(250.dp,60.dp)
+                    .drawBehind {
+                        drawRoundRect(color = Error200, style = stroke)
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                Text(textAlign = TextAlign.Center,text = "Tap here to introduce yourseft")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ButtonsPreview() {
+    ClientTheme {
+        ButtonTest(Modifier.fillMaxSize())
     }
 }
