e0b519c85abc62fa6984b3eb7c36e82185d0f9d6
kimahyeon <dhflxhdxhd@gmail.com>
2024-04-26 23:36:02 +0900

    feat: graphBar 컴포넌트 완 #44

--- web/src/components/atoms/WorkStaticsGraph.tsx
+++ web/src/components/atoms/WorkStaticsGraph.tsx
@@ -1,29 +1,27 @@
 import React, { useRef, useEffect, useState, useMemo } from "react";
 import * as d3 from "d3";
+import styled from "styled-components";
 
 interface DataItem {
   name: string;
   values: number[];
 }
 
-const WorkStaticsGraph = () => {
+interface WorkStaticsGraphProps {
+  data: DataItem[];
+}
+const WorkStaticsGraph = ({ data }: WorkStaticsGraphProps) => {
   const svgRef = useRef(null);
+  const dataRef = useRef(data);
   const width = 500;
   const height = 400;
-
-  const data: DataItem[] = useMemo(
-    () => [
-      { name: "5월 1주", values: [8, 6, 15, 3] },
-      { name: "5월 2주", values: [18, 9, 7, 6] },
-      { name: "5월 3주", values: [12, 2, 9, 8] },
-      { name: "5월 4주", values: [10, 3, 12, 0] },
-      { name: "6월 1주", values: [18, 7, 14, 2] },
-    ],
-    []
-  );
+  const backgroundColor = "#fff";
+  useEffect(() => {
+    dataRef.current = data;
+  }, [data]);
 
   useEffect(() => {
-    if (!svgRef.current) return;
+    if (!svgRef.current || !dataRef.current) return;
 
     const barSvg = d3.select(svgRef.current);
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
@@ -32,34 +30,47 @@ const WorkStaticsGraph = () => {
 
     const xScale = d3
       .scaleBand()
-      .domain(data.map((d) => d.name))
+      .domain(dataRef.current.map((d) => d.name))
       .range([0, innerWidth])
-      .padding(0.2);
+      .padding(0.4);
 
-    const maxVal = d3.max(data, (d) => d3.max(d.values)) ?? 0;
-    console.log(`최대값 ${maxVal}`);
+    // const maxVal = d3.max(dataRef.current, (d) => d3.max(d.values)) ?? 0;
+    const maxVal = 7;
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, maxVal + 15])
+      .domain([0, maxVal + 5])
       .nice()
       .range([innerHeight, 0]);
 
     const xAxis = d3
       .axisBottom(xScale)
-      .tickValues(data.map((d) => d.name))
+      .tickValues(dataRef.current.map((d) => d.name))
+      .tickSize(0)
+      .tickPadding(15);
+
+    const yAxis = d3
+      .axisLeft(yScale)
+      .tickValues([0, 4, 8])
+      .ticks(2)
       .tickSize(0)
       .tickPadding(10);
 
-    const yAxis = d3.axisLeft(yScale);
+    const makeYLines = () => {
+      return d3
+        .axisLeft(yScale)
+        .ticks(2)
+        .tickValues([4, 8])
+        .tickSize(-innerWidth)
+        .tickFormat(() => "");
+    };
 
-    const barWidth = xScale.bandwidth() / data[0].values.length; // 막대 너비 계산
+    const barWidth = xScale.bandwidth() / dataRef.current[0].values.length;
 
-    // 개선된 선택자 사용
     ["day", "eve", "night", "off"].forEach((className, i) => {
       barSvg
         .selectAll(`rect.${className}`)
-        .data(data)
+        .data(dataRef.current)
         .join("rect")
         .attr("class", className)
         .attr("x", (d) => xScale(d.name)! + margin.left + barWidth * i)
@@ -75,23 +86,56 @@ const WorkStaticsGraph = () => {
         .attr("height", (d) => innerHeight - yScale(d.values[i]));
     });
 
-    barSvg
+    const gridLines = barSvg
+      .insert("g", ":first-child")
+      .attr("class", "grid")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .call(makeYLines());
+
+    gridLines
+      .selectAll(".tick line")
+      .attr("stroke", "#ECECEC")
+      .attr("opacity", 0.5);
+
+    gridLines.select(".domain").attr("stroke", backgroundColor);
+
+    const existingXAxis = barSvg.select(".x-axis");
+    if (existingXAxis.node()) {
+      existingXAxis.remove();
+    }
+
+    const existingYAxis = barSvg.select(".y-axis");
+    if (existingYAxis.node()) {
+      console.log("y축 삭제");
+      existingYAxis.remove();
+    }
+
+    const xAxisG = barSvg
       .append("g")
       .attr("class", "x-axis")
       .attr(
         "transform",
         `translate(${margin.left}, ${innerHeight + margin.top})`
       )
-      .call(xAxis)
-      .selectAll("text")
-      .style("text-anchor", "center");
+      .call(xAxis);
 
-    barSvg
+    xAxisG
+      .selectAll("text")
+      .style("text-anchor", "center")
+      .style("fill", "gray")
+      .style("font-size", "12px");
+    xAxisG.select(".domain").attr("stroke", "#ECECEC");
+    const yAxisG = barSvg
       .append("g")
       .attr("class", "y-axis")
       .attr("transform", `translate(${margin.left}, ${margin.top})`)
       .call(yAxis);
-  }, [svgRef, data, width, height]);
+    yAxisG.selectAll("text").style("fill", "gray").style("font-size", "12px");
+    yAxisG.select(".domain").attr("stroke", "#fff");
+    return () => {
+      gridLines.remove();
+    };
+  }, [width, height, data]);
 
   return (
     <>
