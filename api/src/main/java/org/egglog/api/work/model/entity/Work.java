4e207c53c249677138d6f41e3e12ccfb37e71131
김형민 <kimtuti@naver.com>
2024-04-29 18:01:20 +0900

    feat : 근무일정 entity 생성 (#52)

---
+++ api/src/main/java/org/egglog/api/work/model/entity/Work.java
@@ -0,0 +1,51 @@
+package org.egglog.api.work.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.egglog.api.calendargroup.model.entity.CalendarGroup;
+import org.egglog.api.user.model.entity.User;
+import org.egglog.api.worktype.model.entity.WorkType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+/**
+ * ```
+ * ===================[Info]=========================
+ * packageName    : org.egglog.api.work.model.entity
+ * fileName      : Work
+ * description    : 근무 일정 엔티티
+ * =================================================
+ * ```
+ * |DATE|AUTHOR|NOTE|
+ * |:---:|:---:|:---:|
+ * |2024-04-29|김형민|최초 생성|
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Work {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "work_id")
+    private Long id;
+
+    @Column(name = "work_date")
+    private LocalDate workDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "work_type_id")
+    private WorkType workType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calendar_group_id")
+    private CalendarGroup calendarGroup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
