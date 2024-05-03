0dc90c403bb3886a5c5f90bd1586b754951999d4
dohwi <ehgnl97722@ajou.ac.kr>
2024-05-03 17:58:28 +0900

    feat: 검색 리팩터링 중 #61

---
+++ api/src/main/java/org/egglog/api/global/config/CustomMySQL8Dialect.java
@@ -0,0 +1,28 @@
+package org.egglog.api.global.config;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.dialect.MySQLDialect;
+
+/**
+ * ```
+ * ===================[Info]=========================
+ * packageName    : org.egglog.api.global.config
+ * fileName      : CustomMySQL8Dialect
+ * description    :
+ * =================================================
+ * ```
+ * |DATE|AUTHOR|NOTE|
+ * |:---:|:---:|:---:|
+ * |2024-05-03|김도휘|최초 생성|
+ */
+public class CustomMySQL8Dialect extends MySQLDialect {
+
+//    @Override
+//    public void initializeFunctionRegistry(FunctionContributions functionContributions) {
+//        super.initializeFunctionRegistry(functionContributions);
+//        functionContributions.getFunctionRegistry().registerPattern(
+//                "mathes",
+//
+//        )
+//    }
+}
