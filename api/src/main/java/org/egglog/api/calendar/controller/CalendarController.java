806b84152bf8763aa634b3abf81920364e7d15c7
김다희 <dhmonukim24@gmail.com>
2024-05-06 00:42:35 +0900

    chore: 키 이동 단계 추가

--- api/src/main/java/org/egglog/api/calendar/controller/CalendarController.java
+++ api/src/main/java/org/egglog/api/calendar/controller/CalendarController.java
@@ -1,10 +1,12 @@
 package org.egglog.api.calendar.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.egglog.api.calendar.model.dto.params.CalendarMonthRequest;
 import org.egglog.api.calendar.model.service.CalendarService;
 import org.egglog.api.user.model.entity.User;
 import org.egglog.utility.utils.MessageUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +18,18 @@ import java.util.Calendar;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/calendar")
+@Slf4j
 public class CalendarController {
+    @Value("${egg-log.test.value}")
+    private String testVal;
 
     private final CalendarService calendarService;
+
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        log.warn(testVal);
+        return ResponseEntity.ok().body(MessageUtils.success(testVal));
+    }
 
     @GetMapping("/link")
     public ResponseEntity<?> getCalendarUrl(@AuthenticationPrincipal User user) {
