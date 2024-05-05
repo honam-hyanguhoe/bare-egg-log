86c4234c55254ca070d6a67fc8786aa746d7b1b5
김다희 <dhmonukim24@gmail.com>
2024-05-06 03:35:34 +0900

    chore: config server 암호화

--- api/src/main/java/org/egglog/api/global/config/FirebaseConfig.java
+++ api/src/main/java/org/egglog/api/global/config/FirebaseConfig.java
@@ -1,5 +1,6 @@
 package org.egglog.api.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.FirebaseApp;
@@ -7,6 +8,9 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.StorageClient;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.egglog.api.global.util.FirebaseProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,18 +20,22 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class FirebaseConfig {
     // 비밀키 경로 환경 변수
-    @Value("${fcm.config}") private String projectConfig;
+//    @Value("${fcm.config}") private String projectConfig;
     // 프로젝트 아이디 환경 변수
     @Value("${fcm.bucket-name}") private String bucketName;
+    // 비밀키 경로 환경 변수
+    private final FirebaseProperties firebaseProperties;
     // 의존성 주입이 이루어진 후 초기화를 수행한다.
     @PostConstruct
     public void initialize() {
         FirebaseOptions options = null;
         try {
-
-            InputStream credentialsStream = new ByteArrayInputStream(projectConfig.getBytes());
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream credentialsStream = new ByteArrayInputStream(mapper.writeValueAsString(firebaseProperties.getConfig()).getBytes());
             options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(credentialsStream))
                     .setStorageBucket(bucketName)
