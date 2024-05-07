edcc281f6a65bbd2ebcbb8ec13427c94c65b6ab5
김형민 <kimtuti@naver.com>
2024-05-07 15:20:22 +0900

    feat : 보안 적용 (#66)

--- api/src/main/java/org/egglog/api/security/config/SecurityConfig.java
+++ api/src/main/java/org/egglog/api/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import java.util.Collections;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig{
-    private static final String[] ALLOWED_URIS = {"/**"};
+    private static final String[] ALLOWED_URIS = {"/v1/hospital/list", "/v1/auth/login/**", "/v1/auth/refresh", "/test/**"};
     private final JwtFilter jwtFilter;
     private final CustomOAuth2Service customOAuth2Service;
     private final OAuthSuccessHandler oAuth2SuccessHandler;
@@ -43,9 +43,8 @@ public class SecurityConfig{
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(ALLOWED_URIS).permitAll() // 특정 경로 인증 미요구
-//                        .anyRequest().authenticated() // 나머지 경로는 인증 요구
-                                .anyRequest().permitAll()
+                        .requestMatchers(ALLOWED_URIS).permitAll() // 특정 경로 인증 미요구
+                        .anyRequest().authenticated() // 나머지 경로는 인증 요구
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)// JwtFilter 추가
                 .addFilterBefore(exceptionHandlerFilter, JwtFilter.class) // ExceptionHandlerFilter 추가
@@ -61,12 +60,11 @@ public class SecurityConfig{
                                 .userInfoEndpoint(userInfoEndpoint ->
                                         userInfoEndpoint.userService(customOAuth2Service))
                                 .successHandler(oAuth2SuccessHandler)
-//                                .failureHandler(authFailureHandler)
+                                .failureHandler(authFailureHandler)
+                ).exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(authFailureHandler)
                 );
-//                .exceptionHandling(exceptionHandling ->
-//                        exceptionHandling
-//                                .authenticationEntryPoint(authFailureHandler)
-//                );
 
 
         return http.build();
