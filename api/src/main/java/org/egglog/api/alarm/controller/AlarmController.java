0f7d8fcbf37fde36668186aec8a3ea173faf9dbf
김형민 <kimtuti@naver.com>
2024-05-10 11:42:53 +0900

    refactor : 알람 관련 리펙토링 (#66)
    - 알람 서비스 로직 리펙토링
    - 알람 검증 로직 추가

--- api/src/main/java/org/egglog/api/alarm/controller/AlarmController.java
+++ api/src/main/java/org/egglog/api/alarm/controller/AlarmController.java
@@ -1,16 +1,31 @@
 package org.egglog.api.alarm.controller;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.egglog.api.alarm.model.dto.params.AlarmForm;
 import org.egglog.api.alarm.model.dto.params.AlarmModifyForm;
 import org.egglog.api.alarm.model.dto.params.AlarmStatusModifyForm;
 import org.egglog.api.alarm.model.service.AlarmService;
+import org.egglog.api.user.model.entity.User;
 import org.egglog.utility.utils.MessageUtils;
 import org.egglog.utility.utils.SuccessType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
+/**
+ * ```
+ * ===================[Info]=========================
+ * packageName    : org.egglog.api.alarm.controller
+ * fileName      : AlarmController
+ * description    : 알람 컨트롤러
+ * =================================================
+ * ```
+ * |DATE|AUTHOR|NOTE|
+ * |:---:|:---:|:---:|
+ * |2024-04-24|김도휘|최초 생성|
+ * |2024-05-10|김형민|1차 리펙토링|
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/alarms")
@@ -24,19 +39,19 @@ public class AlarmController {
      * @return
      */
     @GetMapping("")
-    public ResponseEntity<?> getAlarmList() {
-//        TODO @AuthenticationPrincipal User user
-        Long userId = 1L;
-        return ResponseEntity.ok().body(MessageUtils.success(alarmService.getAlarmList(userId)));
+    public ResponseEntity<?> getAlarmList(
+            @AuthenticationPrincipal User loginUser
+    ) {
+        return ResponseEntity.ok().body(MessageUtils.success(alarmService.getAlarmList(loginUser)));
     }
 
 
     @PostMapping("")
-    public ResponseEntity registerAlarm(@RequestBody AlarmForm alarmForm
-//            TODO @AuthenticationPrincipal User user){
-    ) {
-        Long userId = null;
-        alarmService.registerAlarm(alarmForm, userId);
+    public ResponseEntity<MessageUtils> registerAlarm(
+            @RequestBody @Valid AlarmForm alarmForm,
+            @AuthenticationPrincipal User loginUser)
+    {
+        alarmService.registerAlarm(alarmForm, loginUser);
         return ResponseEntity.ok().body(MessageUtils.success(SuccessType.NO_CONTENT));
     }
 
@@ -47,11 +62,13 @@ public class AlarmController {
      * @return
      */
     @PatchMapping("/status")
-    public ResponseEntity<?> modifyAlarmStatus(@RequestBody AlarmStatusModifyForm alarmStatusModifyForm) {
-//        TODO @AuthenticationPrincipal User user
-        Long userId = 1L;
-        return ResponseEntity.ok().body(MessageUtils.success(alarmService.modifyAlarmStatus(alarmStatusModifyForm, userId)));
+    public ResponseEntity<?> modifyAlarmStatus(
+            @RequestBody AlarmStatusModifyForm alarmStatusModifyForm,
+            @AuthenticationPrincipal User loginUser
+    ) {
+        return ResponseEntity.ok().body(MessageUtils.success(alarmService.modifyAlarmStatus(alarmStatusModifyForm, loginUser)));
     }
+
 
     /**
      * 알람 정보 변경
@@ -60,10 +77,12 @@ public class AlarmController {
      * @return
      */
     @PatchMapping("")
-    public ResponseEntity<?> modifyAlarm(@RequestBody AlarmModifyForm alarmModifyForm) {
-//        TODO @AuthenticationPrincipal User user
-        Long userId = 1L;
-        return ResponseEntity.ok().body(MessageUtils.success(alarmService.modifyAlarm(alarmModifyForm, userId)));
+    public ResponseEntity<?> modifyAlarm(
+            @RequestBody @Valid AlarmModifyForm alarmModifyForm,
+            @AuthenticationPrincipal User loginUser
+    )
+    {
+        return ResponseEntity.ok().body(MessageUtils.success(alarmService.modifyAlarm(alarmModifyForm, loginUser)));
     }
 
     /**
@@ -73,10 +92,11 @@ public class AlarmController {
      * @return
      */
     @DeleteMapping("/{alarm_id}")
-    public ResponseEntity<?> deleteAlarm(@PathVariable("alarm_id") Long alarmId) {
-//        TODO @AuthenticationPrincipal User user
-        Long userId = 1L;
-        alarmService.deleteAlarm(alarmId);
+    public ResponseEntity<?> deleteAlarm(
+            @PathVariable("alarm_id") Long alarmId,
+            @AuthenticationPrincipal User loginUser
+    ) {
+        alarmService.deleteAlarm(alarmId, loginUser);
         return ResponseEntity.ok().body(MessageUtils.success(SuccessType.DELETE));
     }
 
