69f93d31dd1c303239cbe8ec3374c95b49f946bc
kimahyeon <dhflxhdxhd@gmail.com>
2024-05-10 18:00:36 +0900

    feat: groupList 화면 및 그룹 생성 구현 #77

--- client/data/src/main/java/com/org/egglog/data/group/usecase/GetGroupListUseCaseImpl.kt
+++ client/data/src/main/java/com/org/egglog/data/group/usecase/GetGroupListUseCaseImpl.kt
@@ -1,7 +1,9 @@
 package com.org.egglog.data.group.usecase
 
+import android.util.Log
 import com.org.egglog.data.group.model.toDomainModel
 import com.org.egglog.data.group.service.GroupService
+import com.org.egglog.data.main.model.stats.toDomainModel
 import com.org.egglog.domain.group.model.Group
 import com.org.egglog.domain.group.usecase.GetGroupListUseCase
 import javax.inject.Inject
@@ -9,8 +11,12 @@ import javax.inject.Inject
 class GetGroupListUseCaseImpl @Inject constructor(
     private val groupService: GroupService
 ) : GetGroupListUseCase {
-    override suspend fun invoke(accessToken: String): Result<List<Group>?>  = kotlin.runCatching{
+    override suspend fun invoke(accessToken: String): Result<List<Group>?> = kotlin.runCatching {
+        Log.d("groupList", "accessToken ${accessToken}")
         val response = groupService.getGroupList(accessToken)
+
+        Log.d("groupList", response.dataBody.toString())
         response.dataBody?.mapNotNull { it.toDomainModel() } ?: null
     }
 }
+
