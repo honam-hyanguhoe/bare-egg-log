b21c1bbdb10d672784b5492310385b2b92bf31e5
김형민 <kimtuti@naver.com>
2024-05-10 11:48:31 +0900

    refactor : board request 검증 로직 추가 (#66)

--- api/src/main/java/org/egglog/api/board/model/dto/params/LikeForm.java
+++ api/src/main/java/org/egglog/api/board/model/dto/params/LikeForm.java
@@ -2,6 +2,8 @@ package org.egglog.api.board.model.dto.params;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -10,6 +12,7 @@ import lombok.*;
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public class LikeForm {
-
-    private Long boardId;
+    @NotNull(message = "boardId는 필수 값 입니다.")
+    @Positive(message = "정확한 ID 값을 입력 해주세요.")
+    private Long boardId;//필수
 }
