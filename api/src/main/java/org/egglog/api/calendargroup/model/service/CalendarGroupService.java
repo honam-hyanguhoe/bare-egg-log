89915d60242101bf41e613a03045f761e7a3889f
김다희 <dhmonukim24@gmail.com>
2024-05-12 00:27:22 +0900

    fix: append Transactional annotation

--- api/src/main/java/org/egglog/api/calendargroup/model/service/CalendarGroupService.java
+++ api/src/main/java/org/egglog/api/calendargroup/model/service/CalendarGroupService.java
@@ -19,6 +19,7 @@ import org.egglog.api.user.exception.UserException;
 import org.egglog.api.user.model.entity.User;
 import org.egglog.api.user.repository.jpa.UserJpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -55,6 +56,7 @@ public class CalendarGroupService {
      * @param loginUser
      * @author 김형민
      */
+    @Transactional
     public void deleteCalendarGroup(Long calendarGroupId, User loginUser) {
         log.debug(" ==== ==== ==== [ 캘린더 그룹 삭제 서비스 실행 ] ==== ==== ====");
         if (loginUser.getWorkGroupId()==calendarGroupId) throw new CalendarGroupException(CalendarGroupErrorCode.NOT_CONTROL_WORKGROUP);
--- api/src/main/java/org/egglog/api/group/model/service/GroupMemberService.java
+++ api/src/main/java/org/egglog/api/group/model/service/GroupMemberService.java
@@ -7,6 +7,7 @@ import org.egglog.api.group.model.dto.response.GroupDutySummary;
 import org.egglog.api.group.model.entity.GroupMember;
 import org.egglog.api.group.repository.jpa.GroupMemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
