d06b05eef0fd713397b993e49b4e7307d9c19302
김형민 <kimtuti@naver.com>
2024-05-13 13:44:33 +0900

    fix : fcm 서비스 설정 (#83)

--- api/src/main/java/org/egglog/api/notification/exception/NotificationException.java
+++ api/src/main/java/org/egglog/api/notification/exception/NotificationException.java
@@ -1,5 +1,8 @@
 package org.egglog.api.notification.exception;
 
+import org.egglog.api.inquiry.exception.InquiryErrorCode;
+import org.egglog.utility.exception.BaseException;
+
 /**
  * ```
  * ===================[Info]=========================
@@ -12,5 +15,8 @@ package org.egglog.api.notification.exception;
  * |:---:|:---:|:---:|
  * |2024-05-13|김형민|최초 생성|
  */
-public class NotificationException {
+public class NotificationException extends BaseException {
+    public NotificationException(NotificationErrorCode errorCode){
+        super(errorCode);
+    }
 }
