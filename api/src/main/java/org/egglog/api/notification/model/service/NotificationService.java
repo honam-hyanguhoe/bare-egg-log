b36b62b680bd0ab6b1402b7d63185c3e71da642c
김형민 <kimtuti@naver.com>
2024-05-14 16:09:25 +0900

    feat : 토큰 변경시 토픽 재 구독 구현 (#83)

--- api/src/main/java/org/egglog/api/notification/model/service/NotificationService.java
+++ api/src/main/java/org/egglog/api/notification/model/service/NotificationService.java
@@ -57,55 +57,14 @@ public class NotificationService {
 //    상태 변경
     @Transactional
     public List<NotificationResponse> updateNotification (List<NotificationRequest> requests, User loginUser){
-        //todo : 상태변경 후 해당 구독 취소, 구독 을 하는 로직 필요
+        //todo : 상태변경 후 해당 구독 취소, 구독 을 하는 로직 필요 x -> 구독 유지후 로직으로 알림 발송 하는 방식으로 변경
         Map<Long, UserNotification> notificationMap = notificationRepository.findMapByUser(loginUser);
-        //현재 토픽 알림은 group 만 존재
-        List<GroupPreviewDto> groupByUserId = groupRepository.findGroupByUserId(loginUser.getId());
         List<UserNotification> list = new ArrayList<>();
+
         for (NotificationRequest request : requests) {
-            UserNotification userNotification = notificationMap.get(request.getNotificationId());
-            if (userNotification.getStatus().equals(true)){
-                //구독
-                if (userNotification.getStatus().equals(TopicEnum.GROUP)){
-                    //그룹이면 유저의 모든 그룹의 구독을 추가한다.
-                    for (GroupPreviewDto groupPreviewDto : groupByUserId) {
-                        String loginUserDeviceToken = loginUser.getDeviceToken();
-                        FCMTopic topic = FCMTopic.builder()
-                                .topic(TopicEnum.GROUP)
-                                .topicId(groupPreviewDto.getGroupId())
-                                .build();
-                        if (loginUserDeviceToken!=null){
-                            fcmService.subscribeToTopic(loginUserDeviceToken, topic);
-                            list.add(userNotification.updateState(request.getStatus()));
-                        }else {
-                            list.add(userNotification);
-                        }
-                    }
-                    continue;
-                }
-                list.add(userNotification.updateState(request.getStatus()));
-            }else {
-                //구독 취소
-                if (userNotification.getStatus().equals(TopicEnum.GROUP)){
-                    //그룹이면 유저의 모든 그룹의 구독을 취소한다.
-                    for (GroupPreviewDto groupPreviewDto : groupByUserId) {
-                        String loginUserDeviceToken = loginUser.getDeviceToken();
-                        FCMTopic topic = FCMTopic.builder()
-                                .topic(TopicEnum.GROUP)
-                                .topicId(groupPreviewDto.getGroupId())
-                                .build();
-                        if (loginUserDeviceToken!=null){
-                            fcmService.unsubscribeFromTopic(loginUserDeviceToken, topic);
-                            list.add(userNotification.updateState(request.getStatus()));
-                        }else {
-                            list.add(userNotification);
-                        }
-                    }
-                    continue;
-                }
-                list.add(userNotification.updateState(request.getStatus()));
-            }
+            list.add(notificationMap.get(request.getNotificationId()).updateState(request.getStatus()));
         }
+
         return notificationRepository.saveAll(list)
                 .stream()
                 .map(UserNotification::toResponse)
@@ -121,5 +80,19 @@ public class NotificationService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void updateTopicSubscribeState(String oldToken, User loginUser){
+        //현재 토픽 알림은 그룹 밖에 없다. 나중에 수정 필요.... 전체 토픽으로 설정된 값을 재 구독하는 방식으로..
+        List<GroupPreviewDto> groupByUserId = groupRepository.findGroupByUserId(loginUser.getId());
+        String newToken = loginUser.getDeviceToken();
+        for (GroupPreviewDto groupPreviewDto : groupByUserId) {
+            FCMTopic topic = FCMTopic.builder()
+                    .topic(TopicEnum.GROUP)
+                    .topicId(groupPreviewDto.getGroupId())
+                    .build();
+            fcmService.unsubscribeFromTopic(oldToken, topic); //예전 토큰 구독 취소
+            fcmService.subscribeToTopic(newToken, topic); //새 토큰으로 구독
+        }
+    }
 
 }
--- api/src/main/java/org/egglog/api/notification/repository/jpa/NotificationRepository.java
+++ api/src/main/java/org/egglog/api/notification/repository/jpa/NotificationRepository.java
@@ -2,7 +2,13 @@ package org.egglog.api.notification.repository.jpa;
 
 import org.egglog.api.notification.model.entity.UserNotification;
 
+import org.egglog.api.notification.model.entity.enums.TopicEnum;
+import org.egglog.api.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NotificationRepository extends JpaRepository<UserNotification, Long>, NotificationQueryRepository {
+
+    Optional<UserNotification> findByTypeAndUser(TopicEnum type, User user);
 }
