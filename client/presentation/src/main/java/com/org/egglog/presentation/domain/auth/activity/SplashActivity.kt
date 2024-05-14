61defd0617ee0fb58acebd5e5e8315419c199aae
김수린 <uhyeon1009@gmail.com>
2024-05-14 15:01:16 +0900

    feat: FCM 관련 기본 세팅 완료

--- client/presentation/src/main/java/com/org/egglog/presentation/domain/auth/activity/SplashActivity.kt
+++ client/presentation/src/main/java/com/org/egglog/presentation/domain/auth/activity/SplashActivity.kt
@@ -1,15 +1,20 @@
 package com.org.egglog.presentation.domain.auth.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
-import com.google.firebase.messaging.messaging
-//import com.google.firebase.messaging.messaging
+import com.google.firebase.messaging.ktx.messaging
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.common.KakaoSdk
 import com.navercorp.nid.NaverIdLoginSDK
 import com.org.egglog.domain.auth.model.UserFcmTokenParam
@@ -25,7 +30,6 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 import com.org.egglog.presentation.R
 import com.org.egglog.presentation.domain.main.activity.MainActivity
-import com.org.egglog.presentation.domain.setting.activity.SettingActivity
 import kotlinx.coroutines.tasks.await
 
 @AndroidEntryPoint
@@ -36,19 +40,53 @@ class SplashActivity : AppCompatActivity() {
     @Inject lateinit var setUserStoreUseCase: SetUserStoreUseCase
     @Inject lateinit var updateUserFcmTokenUseCase: UpdateUserFcmTokenUseCase
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
         NaverIdLoginSDK.initialize(this, getString(R.string.naver_client_id), getString(R.string.naver_client_secret), getString(R.string.naver_client_name))
-//        val keyHash = Utility.getKeyHash(this)
-//        Log.e("keyHash: ", keyHash)
-        setContent{
+
+        setContent {
             ClientTheme {
                 SplashScreen()
             }
         }
 
+        // 권한 요청 예시
+        askForPermissions(listOf(
+            Manifest.permission.POST_NOTIFICATIONS,
+            Manifest.permission.INTERNET
+        ))
+    }
+
+    private fun askForPermissions(permissionList: List<String>) {
+        val requestList = ArrayList<String>()
+        for (permission in permissionList) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestList.add(permission)
+            }
+        }
+
+        if (requestList.isNotEmpty()) {
+            for(permission in requestList) requestPermissionLauncher.launch(permission)
+        }
+        startLifecycleScopeWork()
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+
+
+    private fun startLifecycleScopeWork() {
         lifecycleScope.launch {
             val tokens = getTokenUseCase()
             val isLoggedIn = !tokens.first.isNullOrEmpty() && !tokens.second.isNullOrEmpty()
@@ -57,7 +95,7 @@ class SplashActivity : AppCompatActivity() {
                 Log.e("SplashActivity", "refresh: " + tokens.second.orEmpty())
                 val userDetail = getUserUseCase("Bearer ${tokens.first.orEmpty()}").getOrThrow()
                 Log.e("SplashActivity", "user: " + userDetail.toString())
-                if(userDetail?.selectedHospital == null || userDetail.empNo == null) {
+                if (userDetail?.selectedHospital == null || userDetail.empNo == null) {
                     setUserStoreUseCase(userDetail)
                     startActivity(
                         Intent(
@@ -73,7 +111,7 @@ class SplashActivity : AppCompatActivity() {
                         Log.e("FCM Token", "Error fetching FCM token: ${e.message}", e)
                         ""
                     }
-                    if(userDetail.deviceToken == null || userDetail.deviceToken != fcmToken) {
+                    if (userDetail.deviceToken == null || userDetail.deviceToken != fcmToken) {
                         val newUser = updateUserFcmTokenUseCase("Bearer ${tokens.first.orEmpty()}", UserFcmTokenParam(fcmToken)).getOrThrow()
                         setUserStoreUseCase(newUser)
                     } else setUserStoreUseCase(userDetail)
