f97f23e7dff46d94b1221898d2b59bcb67a8ccc5
김수린 <uhyeon1009@gmail.com>
2024-05-17 10:49:14 +0900

    feat: FCM Token 변경 감지

--- client/presentation/src/main/java/com/org/egglog/presentation/receiver/FcmReceiveService.kt
+++ client/presentation/src/main/java/com/org/egglog/presentation/receiver/FcmReceiveService.kt
@@ -12,13 +12,28 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.org.egglog.domain.auth.model.UserFcmTokenParam
+import com.org.egglog.domain.auth.usecase.GetTokenUseCase
+import com.org.egglog.domain.auth.usecase.GetUserStoreUseCase
+import com.org.egglog.domain.auth.usecase.GetUserUseCase
+import com.org.egglog.domain.auth.usecase.SetUserStoreUseCase
+import com.org.egglog.domain.auth.usecase.UpdateUserFcmTokenUseCase
 import com.org.egglog.presentation.R
 import com.org.egglog.presentation.domain.main.activity.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FcmReceiveService : FirebaseMessagingService() {
+    @Inject lateinit var updateUserFcmTokenUseCase: UpdateUserFcmTokenUseCase
+    @Inject lateinit var getTokenStoreUseCase: GetTokenUseCase
+    @Inject lateinit var setUserStoreUseCase: SetUserStoreUseCase
     var TAG:String = "FcmReceiveService"
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -45,8 +60,16 @@ class FcmReceiveService : FirebaseMessagingService() {
         sendRegistrationToServer(token)
     }
 
-    private fun sendRegistrationToServer(token: String) {
-
+    private fun sendRegistrationToServer(deviceToken: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val tokens = getTokenStoreUseCase()
+                val newUser = updateUserFcmTokenUseCase("Bearer ${tokens.first.orEmpty()}", UserFcmTokenParam(deviceToken)).getOrThrow()
+                setUserStoreUseCase(newUser)
+            } catch (e: Exception) {
+                Log.e(TAG, "Error while sending registration to server", e)
+            }
+        }
     }
 
     private fun getBitmapFromUrl(imageUrl: String?): Bitmap? {
@@ -85,7 +108,7 @@ class FcmReceiveService : FirebaseMessagingService() {
         notificationManager.createNotificationChannel(notificationChannel)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.dark)
+            .setSmallIcon(R.drawable.play_store_512)
             .setContentTitle(title)
             .setContentText(messageBody)
             .setAutoCancel(true)
