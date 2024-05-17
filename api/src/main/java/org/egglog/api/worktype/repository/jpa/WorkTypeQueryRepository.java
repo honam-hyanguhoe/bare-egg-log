d1fbcf298cfc5f7eee1ebc16ff43ffec7ef71565
김형민 <kimtuti@naver.com>
2024-05-17 13:18:44 +0900

    fix : 근무 타입 삭제로직 변경 (#83)

--- api/src/main/java/org/egglog/api/worktype/repository/jpa/WorkTypeQueryRepository.java
+++ api/src/main/java/org/egglog/api/worktype/repository/jpa/WorkTypeQueryRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface WorkTypeQueryRepository {
-    List<WorkType> findListWithUserById(Long workTypeId);
+    List<WorkType> findListByUserId(Long userId);
     Optional<WorkType> findWithUserById(Long workTypeId);
 }
